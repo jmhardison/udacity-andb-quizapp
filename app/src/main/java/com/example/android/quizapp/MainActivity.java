fc8c77861bf0215287f865ed1fe35794fda1673a
Jonathan Hardison <jmh@jonathanhardison.com>
2018-01-14 21:47:51 -0600

    added stub methods for score and reset. added id's to questions.

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -2,9 +2,15 @@ package com.example.android.quizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
+
+    //GLOBALS
+    int scoreOfTest = 0;
+    int numberOfQuestions = 10;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,4 +18,16 @@ public class MainActivity extends AppCompatActivity {
         
         setContentView(R.layout.activity_main);
     }
+
+
+    public void scoreButtonPressed(View view){
+
+    }
+
+    public void resetButtonPressed(View view){
+
+    }
+
+
+
 }
